62673a53574a0a51e407e13a61e195b42c729d7c
liyulin <501351981@qq.com>
2018-11-29 14:05:07 +0800

    bug

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {drawImageMixin} from "./drawImage";
 import {drawTextMixin} from "./drawText";
 import {saveMixin} from "./save";
 
-function LiCanvas(canvas_id,options) {
+function LiCanvas(canvas_id,options={}) {
     if(!(this instanceof LiCanvas)){
         throw new Error("LiCanvas 必须通过new关键词进行实例化")
     }
